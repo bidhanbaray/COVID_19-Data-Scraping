b93a7dc9da87df42fc582985b279a5884143b2c5
Sriman Bidhan Baray <35832067+bidhanbaray@users.noreply.github.com>
2020-05-01 22:08:11 +0600

    Update iedcr.py

--- latest_update_summary/iedcr.py
+++ latest_update_summary/iedcr.py
@@ -1,22 +1,13 @@
-import socket
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
 import re
+import xml.etree.ElementTree as ET
 
-mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(('iedcr.gov.bd', 80))
-cmd = 'GET http://iedcr.gov.bd HTTP/1.0\r\n\r\n'.encode()
-mysock.send(cmd)
+webPage = urllib.request.urlopen('https://iedcr.gov.bd/').read()
+soupObject = BeautifulSoup(webPage,'html.parser')
+tags = soupObject('td')
 
-while True:
-    data = mysock.recv(10000)
-    if len(data) < 1:
-        break
-    try:
-        str = data.decode()
-    except:
-        continue
-data = open('data.txt')
-#print(len(str))
-num = re.findall('<td>([0-9]+)</td>',str)
-print('Test conducted in last 24 hours:',num[2],'\nTotal test conducted:',num[5])
-print('COVID-19 positive in last 24 hours:',num[8],'\nTotal COVID-19 Positive:',num[11])
-mysock.close()
+i = 0
+while i<12:
+    print(tags[i].contents[0],tags[i+3].contents[0])
+    i+=4
